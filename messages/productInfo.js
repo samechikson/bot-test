2ac040b8ac193dd9aed43b982b369508c6e74dd3
Sam Echikson <samuel.echikson@gmail.com>
2017-06-13 16:02:54 -0400

    Make productInfo flow use cards and end dialog.

--- messages/productInfo.js
+++ messages/productInfo.js
@@ -7,7 +7,6 @@ module.exports = [
     (session, results, next) => {
         session.send('Ok, this is a %s product', results.response.entity);
         session.dialogData.productNew = (results.response.entity == 'new');
-        console.log(results);
         var msg = new builder.Message(session)
             .text('What category does this product belong to?')
             .suggestedActions(
@@ -23,18 +22,32 @@ module.exports = [
     (session, results, next) => {
         session.send('Ok, you have a %s product', results.response);
         session.dialogData.productCategory = results.response;
+        var msg = new builder.Message(session)
+            .text('Do you have an issue with your product or are you interested in free spools for life?')
+            .suggestedActions(
+                builder.SuggestedActions.create(
+                        session, [
+                            builder.CardAction.imBack(session, 'Product Issue', 'Product Issue'),
+                            builder.CardAction.imBack(session, 'Free Spools', 'Free Spools')
+                        ]
+                    ));
 
-        builder.Prompts.text(session, 'Do you have an issue with your product or are you interested in free spools for life?');
+        builder.Prompts.text(session, msg);
     },
     (session, results, next) => {
-        if (results.response.includes('free')) {
-            builder.Prompts.confirm(session, 'So you want free spools for life, right?');
-            session.dialogData.wantsFreeSpools = true;
+        if (results.response.toLowerCase().includes('free')) {
+            session.replaceDialog('/freeSpools');
         }
-        else if (results.response.includes('issue')) {
+        else if (results.response.toLowerCase().includes('issue')) {
             builder.Prompts.confirm(session, new builder.Message(session).text('So have a problem with your %s, right?', session.dialogData.productCategory));
-            session.dialogData.wantsFreeSpools = false;
         }
+    },
+    (session, result, next) => {
+        console.log(result.response);
+        if (result.response = true){
+            session.send('Ok, here\'s how we are going to fix your problem...');
+        }
+        session.endDialog();
     }
 ];
 
